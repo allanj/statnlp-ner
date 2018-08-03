6879c669f821048b96e1b1a86094d2b6f2b5a1e9
Allan <allanmcgrady@gmail.com>
2018-08-03 19:37:00 +0800

    new bash file

--- script/run.bash
+++ script/run.bash
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-java -cp statnlp-ner-1.0.jar org.statnlp.example.linear_ne.EMain --train_num 500 --dev_num 100 \
-  --test_num 100 --batch true 1 --optimizer sgdclip  \
-  -lstm true -it 50000 --evalDev true 250 \
-  -emb random -os linux -l2 0 > logs/baseline.log 2>&1 
+java -cp statnlp-ner-1.0.jar org.statnlp.example.linear_ne.EMainCRF --train_num 1000 \
+     --test_num 1000 -it 4000 -t 40 --useEmb true --useDiscrete false > logs/emb_only.log 2>&1 
 
